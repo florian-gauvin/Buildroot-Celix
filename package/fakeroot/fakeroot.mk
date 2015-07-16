322ea8c20d3ba417072fe9dd576e27063948e20d
florian-gauvin <florian.g08@hotmail.fr>
2015-07-16 10:50:09 +0200

    fakeroot problem resolved by changing fakeroot.mk

--- package/fakeroot/fakeroot.mk
+++ package/fakeroot/fakeroot.mk
@@ -9,5 +9,6 @@ FAKEROOT_SOURCE = fakeroot_$(FAKEROOT_VERSION).orig.tar.bz2
 FAKEROOT_SITE = http://snapshot.debian.org/archive/debian/20141023T043132Z/pool/main/f/fakeroot
 FAKEROOT_LICENSE = GPLv3+
 FAKEROOT_LICENSE_FILES = COPYING
+HOST_FAKEROOT_CONF_OPTS= --with-ipc=tcp
 
 $(eval $(host-autotools-package))
